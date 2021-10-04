e5c53522d1d318dcfe75bd58c2fa6427e1a9e7a7
Jay <jaystance25@gmail.com>
2021-10-04 14:09:04 -0700

    files

--- studentsstudtportal/settings.py
+++ studentsstudtportal/settings.py
@@ -127,7 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [BASE_DIR/'static']
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Login configuration 
 LOGIN_REDIRECT_URL = "home"
