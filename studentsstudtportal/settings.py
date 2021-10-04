dee72793aae6882c44b35c9661b734ac5fcc7d17
Jay <jaystance25@gmail.com>
2021-10-04 13:54:33 -0700

    updated settings

--- studentsstudtportal/settings.py
+++ studentsstudtportal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,3 +135,4 @@ LOGIN_URL = "login"
 
 # crispy configuration
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+django_heroku.settings(locals())
